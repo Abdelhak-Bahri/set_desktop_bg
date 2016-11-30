a7ad25609133030bafbdd1ff7f724a9d57110f04
Abdelhak Bahri <ca_bahri@esi.dz>
2016-11-30 01:53:03 +0100

    fixing size to 200

--- desktop_bg.py
+++ desktop_bg.py
@@ -14,6 +14,7 @@ key="3847786-5a338eb8002ec90f2352269a4" #get your api key from here : https://pi
 q=""
 image_type="photo"
 size = 0 # number of pictures to download
+per_page="200"
 
 def set_background(path):
     #print "setting ", path , "as bg"
@@ -22,7 +23,7 @@ def set_background(path):
 
 #construct the link to fetch for pictures
 def get_link(query):
-    return base_url+'?key='+key+'&q='+query+'&image_type='+image_type
+    return base_url+'?key='+key+'&q='+query+'&per_page='+per_page
 
 #get images
 def get_images(query, size):
@@ -31,7 +32,15 @@ def get_images(query, size):
         os.makedirs(os.path.dirname(output_dir))
     url = get_link(query)
     #print url
-    get_response= requests.get(url=url)
+    try:
+        get_response= requests.get(url=url)
+        jsonfile = output_dir + query + ".txt"
+        with open(jsonfile, 'w') as json_file:
+            json_file.write(get_response.text)            
+    except requests.exceptions.Timeout:
+        print "timeout or connection error"
+    except requests.exceptions.ConnectionError:
+        print "timeout or connection error"
     if get_response.status_code ==200:
         json_text = get_response.text
         json_object = json.loads(json_text)
@@ -41,16 +50,23 @@ def get_images(query, size):
             #print size
         for i in range(size):
             print i
-            image_url = json_object['hits'][i]['webformatURL']
+            try:
+                image_url = json_object['hits'][i]['webformatURL']
+            except IndexError:
+                print " index error"
+                continue 
             image_url = image_url.replace("_640", "_960")
             #print image_url
             file_name = output_dir + str(json_object['hits'][i]['id']) + ".jpg"
             if not os.path.exists(os.path.abspath(file_name)):
                 print file_name
-                get_image = requests.get(image_url, stream=True)
+                try:
+                    get_image = requests.get(image_url, stream=True)
+                except requests.exceptions.Timeout,requests.exceptions.ConnectionError:
+                    print "timeout or connection error" 
                 if get_image.status_code == 200 :
                     with open(file_name, 'wb') as out_file:
                         shutil.copyfileobj(get_image.raw, out_file)
 		#set_background(output_dir)
 #main call
-get_images(sys.argv[1],size)
+get_images(sys.argv[1],int(per_page))
